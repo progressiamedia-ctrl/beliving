1ed1acf266afc6df7a80be1307f2b7aa01fbae8d
Progressia Development <dev@progressia.app>
2026-05-01 13:20:42 -0400

    fix: remove ThemeProvider to enforce light theme only

--- components/LayoutClient.tsx
+++ components/LayoutClient.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { ThemeProvider } from '@/lib/theme-context'
-
 export function LayoutClient({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider>{children}</ThemeProvider>
+  return <>{children}</>
 }
